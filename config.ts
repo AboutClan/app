44b3dd8372b8f1f7eee475edfffb725e1eb88457
createhb21 <alsghk9701@gmail.com>
2024-07-19 18:56:22 +0900

    add firebase config

---
+++ config.ts
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyBYFfGRL7IGfGCJCX8eQeZlVxankigGsQA',
+  authDomain: 'about-db519.firebaseapp.com',
+  projectId: 'about-db519',
+  storageBucket: 'about-db519.appspot.com',
+  messagingSenderId: '116979215697',
+  appId: '1:116979215697:web:00de4dd16d0f84b76ef770',
+  measurementId: 'G-LPZ00B1RLW',
+};
+
+export {firebaseConfig};
